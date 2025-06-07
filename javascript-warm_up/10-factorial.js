994cf33608fcd8a03b6ff5b270110f4220ed416f
ElvisKS123 <e.shimwa2@alustudent.com>
2025-06-07 11:26:51 +0200

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-  if (n === 1 || isNaN(n)) {
+  if (isNaN(n) || n === 0) {
     return 1;
   }
-
   return n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
