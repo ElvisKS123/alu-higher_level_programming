33fc178aa605d220ec57f9812cc3d0d6781e6520
ElvisKS123 <e.shimwa2@alustudent.com>
2025-06-07 11:24:29 +0200

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
 
-if (isNaN(size)) {
+const square = parseInt(process.argv[2]);
+
+if (isNaN(square)) {
   console.log('Missing size');
-} else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+} else if (square > 0) {
+  const row = 'X'.repeat(square);
+
+  for (let i = 0; i < square; i++) {
+    console.log(row);
   }
 }
